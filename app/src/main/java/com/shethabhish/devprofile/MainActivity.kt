3ff3ac2d118915ee8b67cc4b654beea1d801a2c7
Shethabhish <shethabhish@gmail.com>
2019-06-12 16:54:21 +0530

    Tab

--- app/src/main/java/com/shethabhish/devprofile/MainActivity.kt
+++ app/src/main/java/com/shethabhish/devprofile/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.shethabhish.devprofile
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // to make rounded corners use this codev
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val round = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        round.cornerRadius = 15f
+//        round.isCircular = true   to make the logo completely round, use this line
+        logo.setImageDrawable(round)
     }
 }
